3487124364cb8275d0d4f95dd9f474b4d5153ab9
minerben <144907888+minerben@users.noreply.github.com>
2023-12-05 12:02:55 +0100

    Joining Gathers
    You are now able to join a gather

--- G2G/website/find_gather.py
+++ G2G/website/find_gather.py
@@ -1,24 +1,62 @@
 from flask import Blueprint, request , render_template
-from .models import Gather
+from .models import Gather,User,user_gather_association
 from . import db
+from flask_login import current_user
 
 find_gather = Blueprint('find_gather', __name__)
 
 
-@find_gather.route("/gather_find_angezeigt", methods=['GET', 'POST'])
+@find_gather.route("/angezeigen", methods=['GET', 'POST'])
 def start_page():
 
     if request.method == 'POST':
         print("TEST1")
-        print("TEST1")
-        print("TEST1")   
-        print("TEST1")
+    print("TEST2")
 
-    print("TEST2")
-    print("TEST2")
-    print("TEST2")   
-    print("TEST2")
+
+
+    # new_gather = Gather(name='Gather Name TEST', description='Gather Description', location='Gather Location', Host='Host Name')
+
+    # user1 = User.query.get(1)
+    # user2 = User.query.get(2)
+    # new_gather.users.append(user1)
+    # new_gather.users.append(user2)
+    # db.session.add(new_gather)
+    # db.session.commit()
+
+    # user = User.query.get(1)
+    # joined_gathers = user.Gathers
+
+    # for gather in joined_gathers:
+    #    print(f"Gather Name: {gather.name}, Description: {gather.description}, Location: {gather.location}, Host: {gather.Host}")
+
+
 
     Gathers = Gather.query.order_by(Gather.id).all()
     return render_template('gather_find.html', Gathers=Gathers)
 
+
+@find_gather.route("/join_gather", methods=['POST'])
+def join_gather():
+
+    print("TEST3")
+
+    gather_id = request.form['gather_id']
+
+    print(gather_id)
+
+    gather = Gather.query.get(gather_id)
+
+    user = current_user
+    gather.users.append(user)
+
+    db.session.add(gather)
+    db.session.commit()
+
+    joined_gathers = user.Gathers
+    for gather in joined_gathers:
+        print(f"Gather Name: {gather.name}, Description: {gather.description}, Location: {gather.location}, Host: {gather.Host}")
+
+
+    Gathers = Gather.query.order_by(Gather.id).all()
+    return render_template('gather_find.html', Gathers=Gathers)
--- G2G/website/models.py
+++ G2G/website/models.py
@@ -2,12 +2,19 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+#creating a association between gathers and users
+user_gather_association = db.Table(
+    'user_gather_association',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('gather_id', db.Integer, db.ForeignKey('gather.id'))
+)
+
 #creating Database for login details
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    Gathers = db.relationship('Gather')
+    Gathers = db.relationship('Gather', secondary=user_gather_association, back_populates='users')
    
     #creating a Database for the Gathers
 class Gather(db.Model, UserMixin):
@@ -17,6 +24,7 @@ class Gather(db.Model, UserMixin):
     location = db.Column(db.String(1000))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     Host = db.Column(db.String(20))
+    users = db.relationship('User', secondary=user_gather_association, back_populates='Gathers')
 
     #creating a Database for the map pins
 class Marker(db.Model):
@@ -24,3 +32,4 @@ class Marker(db.Model):
     name = db.Column(db.String(50))
     lat = db.Column(db.Float)
     lng = db.Column(db.Float)
+
