c4f16b4c662d4ad5f0b3248ab032772a4b27797f
KarbonatErolcoded <144907554+KarbonatErolcoded@users.noreply.github.com>
2023-12-13 15:40:46 +0100

    create Gather now pushes Date and Time

--- G2G/website/gather.py
+++ G2G/website/gather.py
@@ -2,39 +2,46 @@ from flask import Blueprint, render_template, request , flash , jsonify , redire
 from . import db
 from .models import Gather, User, Marker, user_gather_association
 from flask_login import current_user, login_required
+from datetime import datetime
 
 gather = Blueprint('gather', __name__)
 
 # create a gather and a marker and add them to databank
 @gather.route('/create',methods=['GET','POST'])
 def create_gather():
-        if request.method == 'POST':
-                name = request.form.get('name')
-                location = request.form.get('location')
-                description = request.form.get('description')
-                currentUser = current_user.id
-                Host = current_user.email
-                latitude = request.form.get('latitude')
-                longitude = request.form.get('longitude')
+    if request.method == 'POST':
+        name = request.form.get('name')
+        location = request.form.get('location')
+        date_str = request.form.get('date')
+        time_str = request.form.get('time')
+        description = request.form.get('description')
+        currentUser = current_user.id
+        Host = current_user.email
+        latitude = request.form.get('latitude')
+        longitude = request.form.get('longitude')
 
+        if len(name) == 0:
+            flash('Please give your Gather a name.', category='error')
+        elif len(location) == 0:
+            flash('Add a location to your Gather.', category='error')
+        elif len(description) == 0:
+            flash('Add a description.', category='error')
+        else:
+            # Convert the date and time strings to date and time objects
+            date_obj = datetime.strptime(date_str, '%Y-%m-%d').date() if date_str else None
+            time_obj = datetime.strptime(time_str, '%H:%M').time() if time_str else None
 
-                if len(name) == 0:
-                        flash('Please give your Gather a name.', category='error')
-                if len(location) == 0:
-                        flash('Add a location to your Gather.', category='error')
-                if len(description) == 0:
-                        flash('Add a description.', category='error')
-                else:        
-                        new_gather = Gather(name=name,description = description, location = location, user_id = currentUser, Host = Host)
-                        db.session.add(new_gather)
-                        db.session.commit()
+            new_gather = Gather(name=name, description=description, location=location, 
+                                Date=date_obj, Time=time_obj, user_id=currentUser, Host=Host)
+            db.session.add(new_gather)
+            db.session.commit()
 
-                        new_marker = Marker(lat=latitude, lng=longitude, gather_id=new_gather.id)
-                        db.session.add(new_marker)
-                        db.session.commit()
+            new_marker = Marker(lat=latitude, lng=longitude, gather_id=new_gather.id)
+            db.session.add(new_marker)
+            db.session.commit()
 
-                        return render_template("gather_find.html")
-                return render_template("creategather.html")                
+            return render_template("gather_find.html")
+        return render_template("creategather.html")                
 
 #refreshing the gather-finding-page to show the gathers
 @gather.route("/anzeigen", methods=['GET', 'POST'])
